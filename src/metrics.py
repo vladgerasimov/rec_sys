704c2508709b923513b484fc3468285e02ee0e31
vlad <lector45rus@gmail.com>
2021-11-13 10:09:34 +0300

    final_project

---
+++ src/metrics.py
@@ -0,0 +1,125 @@
+import numpy as np
+
+def precision(recommended_list, bought_list):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    precision = flags.sum() / len(recommended_list)
+    
+    return precision
+
+
+def precision_at_k(recommended_list, bought_list, k=5):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    bought_list = bought_list  # Тут нет [:k] !!
+    recommended_list = recommended_list[:k]
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    precision = flags.sum() / len(recommended_list)
+    
+    
+    return precision
+
+
+def money_precision_at_k(recommended_list, bought_list, prices_recommended, k=5):
+        
+    # your_code
+    # Лучше считать через матричное произведение, а не цикл
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    prices_recommended = np.array(prices_recommended)
+    
+    recommended_list = recommended_list[:k]
+    prices_recommended = prices_recommended[:k]
+    
+    # Почему здесь изменен порядок в функции isin?
+    flags = np.isin(recommended_list, bought_list)
+    
+    relevant_revenue = (flags * prices_recommended).sum()
+    recommended_revenue = prices_recommended.sum()
+    
+    precision = relevant_revenue / recommended_revenue
+    
+    return precision
+
+def recall(recommended_list, bought_list):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    recall = flags.sum() / len(bought_list)
+    
+    return recall
+
+
+def recall_at_k(recommended_list, bought_list, k=5):
+    
+    # your code
+    recall_at_k = recall(recommended_list[:k], bought_list)
+    
+    return recall_at_k
+
+
+def money_recall_at_k(recommended_list, bought_list, prices_recommended, prices_bought, k=5):
+    
+    # your_code
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    prices_recommended = np.array(prices_recommended)
+    prices_bought = np.array(prices_bought)
+
+    
+    assert recommended_list.shape == prices_recommended.shape
+    assert bought_list.shape == prices_bought.shape
+    
+    
+    recommended_list = recommended_list[:k]
+    prices_recommended = prices_recommended[:k]
+    
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    money_recall = (flags * prices_bought).sum() / (prices_bought).sum()
+    
+    return money_recall
+
+
+def ap_k(recommended_list, bought_list, k=5):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+
+    flags = np.isin(recommended_list, bought_list)
+
+    if sum(flags) == 0:
+        return 0
+
+    sum_ = 0
+    for i in range(k):
+        if flags[i] == True:
+            p_k = precision_at_k(recommended_list, bought_list, k=i + 1)
+            sum_ += p_k
+
+    result = sum_ / min(len(recommended_list), k)
+
+    return result
+
+
+def map_k(recommended_list, bought_list, k=5):
+    # recommended_list = np.array(recommended_list)
+    # bought_list = np.array(bought_list)
+
+    ap_s = [ap_k(rec, bought, k) for rec, bought in zip(recommended_list.values, bought_list.values)]
+
+    return sum(ap_s) / len(recommended_list)
+
+
+print(1)
---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+
+def prefilter_items(data, take_n_popular=5000):
+    # Уберем самые популярные товары (их и так купят)
+    popularity = data.groupby('item_id')['user_id'].nunique().reset_index() / data['user_id'].nunique()
+    popularity.rename(columns={'user_id': 'share_unique_users'}, inplace=True)
+
+    # Возьмем топ по популярности
+    popularity = data.groupby('item_id')['quantity'].sum().reset_index()
+    popularity.rename(columns={'quantity': 'n_sold'}, inplace=True)
+
+    popularity.index = popularity.index.astype('int')
+    top = popularity.sort_values('n_sold', ascending=False).head(int(take_n_popular)).item_id.tolist()
+    
+    # Заведем фиктивный item_id (если юзер покупал товары из топ-5000, то он "купил" такой товар)
+    data.loc[~data['item_id'].isin(top), 'item_id'] = 999999
+
+    return data
+
